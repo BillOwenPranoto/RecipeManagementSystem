53fc53ade5d2a88bb4792de5dec476f7cd05568f
BillOwenPranoto <pranotobillowen@gmail.com>
2025-08-09 22:03:24 +0800

    Removed unused library.

--- src/main/kotlin/classes/RecipeIngredient.kt
+++ src/main/kotlin/classes/RecipeIngredient.kt
@@ -1,6 +1,5 @@
 package classes
 import enums.MeasurementUnit
-import jdk.jfr.DataAmount
 
 data class RecipeIngredient(val ingredient: Ingredient, val amount: Double,
                        var notes: String?, val unit: MeasurementUnit) {
